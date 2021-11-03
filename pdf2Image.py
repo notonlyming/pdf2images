caa0f34115f2ffbb81ef104fff7724e5812be7d4
LittleMing <notonlyming@qq.com>
2021-11-03 17:02:03 +0800

    Add files via upload

--- pdf2Image.py
+++ pdf2Image.py
@@ -18,6 +18,7 @@ def pdf_image(pdfPath, imgPath, zoom_x=5, zoom_y=5, rotation_angle=0):
     pdf = fitz.open(pdfPath)
     # 逐页读取PDF
     for pg in range(0, pdf.pageCount):
+        print(f'第{pg+1}页转图像，共{pdf.pageCount}页...')
         page = pdf[pg]
         # 设置缩放和旋转系数
         trans = fitz.Matrix(zoom_x, zoom_y).prerotate(rotation_angle)
@@ -38,29 +39,39 @@ def images_merge(images, images_number_in_one=2):
     LINE_PIXEL = 3 # 细线边框的厚度
     pages_number = math.ceil(len(images) / images_number_in_one) # 合并后的总页数
     pages_images = list() # 所有合并后的页面
+    single_image_size = Image.open(io.BytesIO(images[0])).size # 临时打开第一个图像获取尺寸
     for page_index in range(pages_number):
+        print(f'第{page_index+1}页合并图像，共{pages_number}页...')
         # 新建空画布
-        im = Image.open(io.BytesIO(images[0])) # 临时打开第一个图像获取尺寸
-        pages_images.append(Image.new('RGB', (im.size[0]*images_number_in_one + ONE_CM_PIXEL*3, im.size[1] + ONE_CM_PIXEL*2), color='White'))
+        pages_images.append(Image.new('RGB', 
+        (
+            single_image_size[0]*images_number_in_one + ONE_CM_PIXEL*3, 
+            single_image_size[1] + ONE_CM_PIXEL*2), color='White'
+        )
+        )
         for little_page_index in range(images_number_in_one):
+            # 检测最后一页，如果超范围就跳出
+            if page_index * images_number_in_one + little_page_index >= len(images):
+                break
+
             # 打开图像
             im = Image.open(io.BytesIO(images[page_index * images_number_in_one + little_page_index]))
             # 在图像四周绘制边框
             ImageDraw.Draw(im).line(
                 [
-                    (0+LINE_PIXEL, 0+LINE_PIXEL), (im.size[0]-LINE_PIXEL, 0+LINE_PIXEL),
-                    (0+LINE_PIXEL, 0+LINE_PIXEL), (0+LINE_PIXEL, im.size[1]-LINE_PIXEL),
-                    (0+LINE_PIXEL, im.size[1]-LINE_PIXEL), (im.size[0]-LINE_PIXEL, im.size[1]-LINE_PIXEL),
-                    (im.size[0]-LINE_PIXEL, im.size[1]-LINE_PIXEL), (im.size[0]-LINE_PIXEL, 0+LINE_PIXEL)
+                    (0+LINE_PIXEL, 0+LINE_PIXEL), (single_image_size[0]-LINE_PIXEL, 0+LINE_PIXEL),
+                    (0+LINE_PIXEL, 0+LINE_PIXEL), (0+LINE_PIXEL, single_image_size[1]-LINE_PIXEL),
+                    (0+LINE_PIXEL, single_image_size[1]-LINE_PIXEL), (single_image_size[0]-LINE_PIXEL, single_image_size[1]-LINE_PIXEL),
+                    (single_image_size[0]-LINE_PIXEL, single_image_size[1]-LINE_PIXEL), (single_image_size[0]-LINE_PIXEL, 0+LINE_PIXEL)
                 ],
                 fill='Black', width=LINE_PIXEL
                 )
             # 图像放到空画布中
             pages_images[page_index].paste(im, (
-                ONE_CM_PIXEL*(little_page_index+1) + im.size[0]*little_page_index, 
+                ONE_CM_PIXEL*(little_page_index+1) + single_image_size[0]*little_page_index, 
                 ONE_CM_PIXEL, 
-                ONE_CM_PIXEL*(little_page_index+1) + im.size[0]*(little_page_index+1), 
-                ONE_CM_PIXEL+im.size[1]
+                ONE_CM_PIXEL*(little_page_index+1) + single_image_size[0]*(little_page_index+1), 
+                ONE_CM_PIXEL+single_image_size[1]
                 ))
         #pages_images[page_index].show()
     return pages_images
@@ -68,6 +79,7 @@ def images_merge(images, images_number_in_one=2):
 path = sys.argv[1]
 pdf_png_pages = pdf_image(path, path)
 pages = images_merge(pdf_png_pages, 2)
+print('写出文件：')
 for page_index in range(len(pages)):
     print(f'{path}{page_index+1}.png')
     pages[page_index].save(f'{path}{page_index+1}.png')
